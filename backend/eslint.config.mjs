41cbe5dbd0449bac457cba0b7fd57ca82fc1f942
katya-tsepelieva <katyatsepelieva@gmail.com>
2025-04-10 22:11:27 +0300

    Set up ESLint with basic rules and ignore patterns for the project

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "semi": ["error", "always"], 
+      "no-console": "off", 
+      "curly": ["error", "all"], 
+    },
+    ignores: [
+      "package-lock.json",
+      "*.min.js", 
+      ".vscode/",             
+      "node_modules/",         
+      ".env",                  
+    ]
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser }
+  },
+]);
